02fdc0ba689835de636698cccb19ef3b322e9d2f
Silvio <jalcoser1996@gmail.com>
2023-04-17 07:02:56 -0500

    Video 6

---
+++ Application/ServiceExtencions.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using FluentValidation;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class ServiceExtencions
+    {
+        public static void AddAplicationLayer(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg=>cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        }
+    }
+}
