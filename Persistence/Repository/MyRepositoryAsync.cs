5089b6d658a404278f0210c5b22647f27c101ee6
Silvio <jalcoser1996@gmail.com>
2023-04-17 23:32:39 -0500

    Repository Pattern y Persistence

---
+++ Persistence/Repository/MyRepositoryAsync.cs
@@ -0,0 +1,17 @@
+﻿using Application.Interfaces;
+using Ardalis.Specification.EntityFrameworkCore;
+using Persistence.Context;
+
+namespace Persistence.Repository;
+
+public class MyRepositoryAsync<T>:RepositoryBase<T>, IRepositoryAsync<T> where T : class
+{
+    private readonly ApplicationDbContext dbsContext;
+
+    public MyRepositoryAsync(ApplicationDbContext dbsContext):base(dbsContext)
+    {
+        this.dbsContext = dbsContext;
+    }
+
+    
+}
